6bab5e4178c57a3ff1316179fd6ba3861b6b6c4d
DamianL96 <damianlambrecht@gmail.com>
2024-03-05 11:41:18 -0300

    union de comandos de inicio de backend y frontend

--- src/app/services/usuarios-db.service.ts
+++ src/app/services/usuarios-db.service.ts
@@ -11,7 +11,7 @@ import { partida } from 'src/app/interfaces/interface-partida';
 })
 export class UsuariosDbService implements OnInit{
 
-  url: string="http://localhost:4200/usuarios";
+  url: string="http://localhost:3001/usuarios";
   listado: usuario[]|undefined=[];
 
   
